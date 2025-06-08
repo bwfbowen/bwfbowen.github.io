492262e5f636ff9b4875524a97e55fda47e90c76
bwfbowen <104526323+bwfbowen@users.noreply.github.com>
2025-06-08 04:31:00 +0000

    Deploying to gh-pages from @ bwfbowen/bwfbowen.github.io@527f4e4e88b8694d3ea100c23edef64def788d3c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -504,13 +504,6 @@ ninja.data = [{
           window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/bwfbowen", "_blank");
-        },
-      },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
